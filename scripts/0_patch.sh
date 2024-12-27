0048bf75719ee8fcf1c8c2f53aa944496336d4d8
Thinh Vu <thinhvux@protonmail.com>
2024-12-27 10:47:46 +0700

    Running in GPU

--- scripts/0_patch.sh
+++ scripts/0_patch.sh
@@ -8,13 +8,13 @@ source .dfl/deep/bin/activate
 
 set -e
 
-python3.11 -m pip install --upgrade pip numpy==1.23.3 numexpr opencv-python colorama tqdm future tensorflow-macos scipy pillow PyQt5
+python3.11 -m pip install --upgrade pip numpy==1.23.5 numexpr opencv-python colorama tqdm future tensorflow-macos tensorflow-metal scipy pillow PyQt5
 
 source .dfl/env/bin/activate
 
 set -e
 
-python3.10 -m pip install --upgrade pip numpy==1.23.3 numexpr opencv-python colorama tqdm future tensorflow-macos scipy pillow PyQt5 onnx protobuf>=3.12.2
+python3.10 -m pip install --upgrade pip numpy==1.23.5 numexpr opencv-python colorama tqdm future tensorflow-macos tensorflow-metal scipy pillow PyQt5 onnx protobuf>=3.12.2
 
 cp -R .dfl/ffmpeg/ffmpeg .dfl/deep/lib/python3.11/site-packages/
 
